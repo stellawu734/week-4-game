e26dc44831a6ab63d5438c16f539b3281bcaa6cc
Stella Wu <stellawu@utexas.edu>
2016-08-21 13:27:33 -0500

    basic structure done

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+//variables: HP, attack power, enemies left
+var myHP;
+var myAttackPower;
+var enemiesLeft;
+var enemyHP;
+var enemyAttackPower;
+//onlick 1: your character, animate, the rest go to ememies, change background color, set HP
+//and attack power
+//onclick 2: enemy 1, animate, get HP and attack power
+//Attack button: on click, calculate HP with attack power until one HP gets to or below 0
+//if you die, game over and restart button appear
+//if you win, choose between the rest 2 enemies, animate, set HP and attach power, repeat
+//if you win all three, display message and restart button
+//restart button
